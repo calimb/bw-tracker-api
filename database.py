87f8acb781f6a232058cf730000c3150ff6bd12a
calimb <maxime.bon4@gmail.com>
2026-08-10 22:56:54 +0200

    switch to postgresql

--- database.py
+++ database.py
@@ -1,17 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
 # =====================================================
 # CONFIGURATION BASE DE DONNÉES
 # =====================================================
 
-# Fichier SQLite local → sport_app.db créé automatiquement
-DATABASE_URL = "sqlite:///./sport_app.db"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}  # nécessaire pour SQLite
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./sport_app.db"
 )
+
+# Render utilise postgres:// mais SQLAlchemy nécessite postgresql://
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(DATABASE_URL)
 
 # Classe de base dont hériteront tous vos modèles
 Base = declarative_base()
